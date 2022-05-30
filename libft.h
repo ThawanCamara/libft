013825faef1661425904c5767ac8f07f1f5898f4
thawancamara <thawancamara@gmail.com>
2022-05-30 19:58:46 -0300

    first commit, mostly empty functions

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tde-souz <tde-souz@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 19:39:56 by tde-souz          #+#    #+#             */
+/*   Updated: 2022/05/30 19:39:56 by tde-souz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include<stdlib.h>
+
+
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+
+size_t	ft_strlen(const char *s);
+
+
+#endif
